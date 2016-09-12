25e9939e8647856616ba4101d9b23846c15ec0b9
buckwheatthedog <nsteigmann@cca.edu>
2016-09-11 21:17:15 -0700

    Create Puzzle1.js

---
+++ Puzzle1.js
@@ -0,0 +1,29 @@
+right();
+down();
+
+function snake() {
+  down();
+  down(); 
+  down();
+  down(); 
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+  right(); 
+  right();
+  right();
+  right();
+  up();
+  up();
+  up();
+  up();
+  right();
+  right();
+
+}
+
+
+snake();
